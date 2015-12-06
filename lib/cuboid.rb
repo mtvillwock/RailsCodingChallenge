5d3a10764ad28a37d876c3d8d00cd932405a3fee
Matthew Villwock <mtvillwock@gmail.com>
2015-12-06 13:06:31 -0800

    add test, rm driver code

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -20,16 +20,10 @@ class Cuboid
     @x = x
     @y = y
     @z = z
+    [@x, @y, @z]
   end
 
   def vertices
-    # given origin (x,y,z) and length, width, and height
-    # return 8 vertices (no repeats) (origin -> 3 adjacent -> 2 adjacent -> final)
-    # add length, width, and height to origin points (x,y,z) to find diagonal vertex
-    # add length to origin, add width to origin, add height to origin
-    # total will be vertices (3 of them)
-    # repeat process with new 3 vertices to find 2 new vertices
-    # repeat process to find final vertex
     origin = [@x, @y, @z] # bottom, left, front
     diagonal_vertex = [@x + @width, @y + @height, @z + @length] # top, right, back
 
@@ -41,8 +35,6 @@ class Cuboid
     g = [@x + @width, @y, @z + @length] # bottom, right, back
 
     known_vertices = [origin, b, c, d, e, f, g, diagonal_vertex]
-    puts "known_vertices are #{known_vertices}"
-    known_vertices
   end
 
   #returns true if the two cuboids intersect each other.  False otherwise.
@@ -64,17 +56,3 @@ class Cuboid
 
   #END public methods that should be your starting point
 end
-
-cuboid_hash = { x: 2, y: 3, z: 4, width: 3, height: 4, length: 5 }
-c = Cuboid.new(cuboid_hash)
-c.vertices
-
-cuboid_hash2 = { x: 3, y: 1, z: 2, width: 4, height: 2, length: 3 }
-c2 = Cuboid.new(cuboid_hash2)
-c.vertices
-
-p c.intersects?(c2)
-
-c3 = Cuboid.new({ x: 1, y:1, z: 1, width: 2, height: 2, length: 2})
-c4 = Cuboid.new({ x: 2, y:2, z: 2, width: 2, height: 2, length: 2})
-p c4.intersects?(c3)
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,30 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
+
   describe "move_to" do
+    subject = Cuboid.new({x: 0, y: 0, z: 0, length: 2, width: 3, height: 4})
     it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+      subject.move_to!(1,2,3)
+      expect(subject.x).to be 1
+      expect(subject.y).to be 2
+      expect(subject.z).to be 3
     end
-  end    
-  
+  end
+
   describe "intersects?" do
+    non_overlapping_cube = Cuboid.new({x: 3, y: 3, z: 2, length: 2, width: 3, height: 4})
+    overlapping_cube = Cuboid.new({x: 1, y: 1, z: 1, length: 2, width: 3, height: 4})
+    subject = Cuboid.new({x: 0, y: 0, z: 0, length: 2, width: 3, height: 4})
+    it "returns true when there is an overlap" do
+      expect(subject.intersects?(overlapping_cube)).to be true
+    end
+    it "returns false when there is no overlap" do
+      expect(subject.intersects?(non_overlapping_cube)).to be false
+    end
   end
 
 end
